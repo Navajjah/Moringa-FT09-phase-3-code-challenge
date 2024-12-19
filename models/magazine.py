acab3e96feb51d5cd76bf2510abced817d443034
Debrah Navajjah Muinde <debbynav645@gmail.com>
2024-12-19 11:55:39 +0300

    Updates Changes

--- models/magazine.py
+++ models/magazine.py
@@ -1,8 +1,35 @@
 class Magazine:
-    def __init__(self, id, name, category):
-        self.id = id
+    def __init__(self, id, name, category=None):
+        self._id = id
         self.name = name
-        self.category = category
+        self.category = category or "General"
+    
+    @property
+    def id(self):
+        return self._id
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if 2 <= len(value) <= 16:
+            self._name = value
+        else:
+            print(f"Invalid new asssignment. '{value}' is not a valid string.") 
+    
+    @property
+    def category(self):
+        return self._category
+    
+    @category.setter
+    def category(self, value):
+        if len(value) > 0:
+            self.category = value
+        else:
+            print(f"Invalid new asssignment. '{value}' is not a valid string.")
+
 
     def __repr__(self):
         return f'<Magazine {self.name}>'
