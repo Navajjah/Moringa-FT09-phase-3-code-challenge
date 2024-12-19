96327860249101b2d1353f217c09ba05d57657d4
Debrah Navajjah Muinde <debbynav645@gmail.com>
2024-12-19 12:03:17 +0300

    Modifies files

--- models/magazine.py
+++ models/magazine.py
@@ -1,35 +1,34 @@
 class Magazine:
     def __init__(self, id, name, category=None):
         self._id = id
-        self.name = name
-        self.category = category or "General"
-    
+        self._name = name
+        self._category = category or "General"
+
     @property
     def id(self):
         return self._id
-    
+
     @property
     def name(self):
         return self._name
-    
+
     @name.setter
     def name(self, value):
         if 2 <= len(value) <= 16:
             self._name = value
         else:
-            print(f"Invalid new asssignment. '{value}' is not a valid string.") 
-    
+            raise ValueError("Name must be between 2 and 16 characters.")
+
     @property
     def category(self):
         return self._category
-    
+
     @category.setter
     def category(self, value):
         if len(value) > 0:
-            self.category = value
+            self._category = value
         else:
-            print(f"Invalid new asssignment. '{value}' is not a valid string.")
-
+            raise ValueError("Category must not be empty.")
 
     def __repr__(self):
         return f'<Magazine {self.name}>'
